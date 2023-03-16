45486110af949e8bd328ca396f0095f64d8e7cea
Webcoder-Sevvy <sevvy__dhiman@outlook.com>
2023-03-17 00:14:33 +0530

    CSS Styles & Footer Links update

--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -7,16 +7,16 @@ const Footer = () => {
         <footer className="Footer__Section">
             <small className="Footer__Copyright">&copy; 2023 Sevvy's Travel Journal</small>
             <div className="Footer__Links-Container">
-                <a href="https://www.linkedin.com/in/webcodersevvy/" className="Footer__Follow-Link">
+                <a href="https://www.linkedin.com/in/webcodersevvy/" target={"_blank"} rel="noreferrer" className="Footer__Follow-Link">
                     <FaLinkedin/>
                 </a>
-                <a href="https://www.instagram.com/webcodersevvy/" className="Footer__Follow-Link">
+                <a href="https://www.instagram.com/webcodersevvy/" target={"_blank"} rel="noreferrer" className="Footer__Follow-Link">
                     <FaInstagramSquare/>
                 </a>
-                <a href="https://www.twitter.com/webcodersevvy/" className="Footer__Follow-Link">
+                <a href="https://www.twitter.com/webcodersevvy/" target={"_blank"} rel="noreferrer" className="Footer__Follow-Link">
                     <FaTwitterSquare/>
                 </a>
-                <a href="https://github.com/sevvy-thewebcoder" className="Footer__Follow-Link">
+                <a href="https://github.com/sevvy-thewebcoder" target={"_blank"} rel="noreferrer" className="Footer__Follow-Link">
                     <FaGithubSquare/>
                 </a>
             </div>
